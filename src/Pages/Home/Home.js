7a3bd5bd6569c89bd9470123939f7b57376f1b7d
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-05-24 18:03:26 +0600

    simple navbar created

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-center text-6xl'>Welcome to Golden Weight Tools</h1>
+        </div>
+    );
+};
+
+export default Home;
