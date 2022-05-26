396e7476b8850540b4cd4a5d1168d55411cb1312
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-05-26 08:56:51 +0600

    portfolio added

--- src/Pages/MyPortfolio/MyPortfolio.js
+++ src/Pages/MyPortfolio/MyPortfolio.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import user from '../../assets/icon/Places-user-identity-icon.png';
+import html from '../../assets/skills/html-5-icon.png';
+import css from '../../assets/skills/css-3-icon.png';
+import javascript from '../../assets/skills/javascript-icon.png';
+import react from '../../assets/skills/react.png';
 
 const MyPortfolio = () => {
     return (
-        <div>
-            <h1>This is my Portfolio.</h1>
+        <div className='my-12'>
+            <div>
+                <img className='mx-auto border-2 border-slate-400 rounded-md p-3' src={user} alt="" />
+            </div>
+            <div className='text-xl text-center mt-4'>
+                <p><span className='font-semibold'>Name: </span> Md. Mobashirul Alam Seam</p>
+                <p><span className='font-semibold'>Email : </span> mobashirulalamseam@gmail.com</p>
+                <p className='w-1/2 mx-auto'><span className='font-semibold'>Educational Info : </span>
+                    I've passed SSC at 2017 as a student of Chittagong Govt. High School, Chittagong under Chittagong Board.
+                    I've completed HSC at 2019 as a student of Hajera Taju Degree College, Chittagong under Chittagong Board.
+                    Now, I'm continuing my study in "Bachelor of Science(BSc.) in Mathematics" from Chittagong College, Chittagong under National University, Gazipur.
+                </p>
+            </div>
+            <div className='flex justify-center gap-8 mt-8'>
+                <h1 className='text-xl text-center font-semibold my-auto'>My Skills :</h1>
+                <div><img src={html} alt="" /></div>
+                <div><img src={css} alt="" /></div>
+                <div><img src={javascript} alt="" /></div>
+                <div><img width={88} className='mt-1' src={react} alt="" /></div>
+            </div>
+            <div className='text-center mt-8'>
+                <p className='font-semibold text-2xl underline mb-2'>Some of my Project Live site</p>
+                <div className='flex flex-col text-lg text-primary'>
+                    <a target='_blank' rel="noreferrer" href="https://fitness-pro-equipment-house.web.app/">
+                        https://fitness-pro-equipment-house.web.app/
+                    </a>
+                    <a target='_blank' rel="noreferrer" href="https://pro-fit-personal-trainer.web.app/">
+                        https://pro-fit-personal-trainer.web.app/
+                    </a>
+                    <a target='_blank' rel="noreferrer" href="https://golden-profiterole-0fa645.netlify.app/">
+                        https://golden-profiterole-0fa645.netlify.app/
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
