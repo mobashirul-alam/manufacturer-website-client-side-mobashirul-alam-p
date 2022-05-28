6c1e13ce4fa0bb94f2a5e1ba23d3203f14b6cccd
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-05-28 12:50:31 +0600

    reviews added and loaded in home

--- src/Pages/Dashboard/AddReview.js
+++ src/Pages/Dashboard/AddReview.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 const AddReview = () => {
+    const [user, loading] = useAuthState(auth);
+    const { register, handleSubmit, reset } = useForm();
+
+    const onSubmit = data => {
+        console.log(data)
+        fetch('http://localhost:5000/reviews', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result) {
+                    toast('Thank you for your significant Review');
+                    reset();
+                }
+            })
+    };
+
+    if (loading) {
+        return <Loading></Loading>;
+    }
+
     return (
-        <div>
-            <h1>Add Review</h1>
+        <div className='text-center shadow-xl p-8 rounded-xl md:mt-12'>
+            <h1 className='text-center text-3xl font-medium mb-4'>Review Form</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" placeholder="User Name" value={user.displayName} class="input input-bordered w-80 mb-2" required {...register("userName")} />
+                <select class="select select-bordered w-full max-w-xs mb-2" required {...register("rating")}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <br />
+                <textarea class="textarea textarea-bordered w-80" placeholder="Please Comment" required {...register("comment")}></textarea>
+                <br />
+                <input type="submit" value='Place Review' className='btn mt-2' />
+            </form>
         </div>
     );
 };
--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Reviews = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/reviews')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
     return (
         <div>
-            <h1 className='text-center text-5xl my-12'>Customers Talk ...</h1>
+            <h1 className='text-center text-5xl mt-20'>Customers Talk ...</h1>
+            <hr className='border border-accent w-4/5 lg:w-1/2 mx-auto mt-6 mb-8' />
+            <div className='flex flex-wrap justify-center gap-10'>
+                {
+                    reviews.map(r => <>
+                        <div class="card w-96 bg-base-100 shadow-2xl">
+                            <div class="card-body mx-auto">
+                                <h2 class="card-title text-green-700">{r.userName}</h2>
+                                <hr />
+                                <h2 class="card-title text-primary  text-base font-bold">Rating : {r.rating}</h2>
+                                <hr />
+                                <p>{r.comment}</p>
+                            </div>
+                        </div>
+                    </>)
+                }
+            </div>
         </div>
     );
 };
